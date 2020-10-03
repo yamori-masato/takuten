8714f3470dfbc27fbeec2fe57e3ff262011e6ba1
yamori-masato <yamorimasato@gmail.com>
2020-10-04 00:57:50 +0900

    ExceptionTimeモデルのバリデーションを追加

--- app/models/exception_time.rb
+++ app/models/exception_time.rb
@@ -3,7 +3,19 @@ class ExceptionTime < ApplicationRecord
         validates :date
         validates :recurring_id
     end
+    validate :validate_match_recurring_pattern
 
     belongs_to :recurring
 
+
+    private
+        # dateがRegularの繰り返し内に含まれるか
+        def validate_match_recurring_pattern
+            return if errors.full_messages.present?
+            
+            parent = Activity::Regular.find(self.recurring_id)
+            unless parent.occurs_on?(self.date)
+                errors.add(:base, "#date(#{date})が正規コマに一致しません")
+            end
+        end
 end
